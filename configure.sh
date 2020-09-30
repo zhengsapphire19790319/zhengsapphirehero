e41a7e2a67a32fd77612fa1bf4e644d84b2dd7d6
zhengsapphire19790319 <71797624+zhengsapphire19790319@users.noreply.github.com>
2020-09-30 20:31:00 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -5,9 +5,10 @@ mkdir /tmp/v2ray
 curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
 install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
-#install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
-chmod +x /tmp/v2ray/v2ctl
+install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
 
+# Remove temporary directory
+rm -rf /tmp/v2ray
 
 # V2Ray new configuration
 install -d /usr/local/etc/v2ray
@@ -38,10 +39,8 @@ cat << EOF > /usr/local/etc/v2ray/config.json
     ]
 }
 EOF
-/tmp/v2ray/v2ct config /usr/local/etc/v2ray/config.json > /usr/local/etc/v2ray/config.pb
 
-# Remove temporary directory
-rm -rf /tmp/v2ray
 
 # Run V2Ray
+/usr/local/bin/v2ctl config /usr/local/etc/v2ray/config.json > /usr/local/etc/v2ray/config.pb
 /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.pb -f pb
