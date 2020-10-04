3ddd55276ee83e484c23dd71950082bd89b220bd
zhengsapphire19790319 <71797624+zhengsapphire19790319@users.noreply.github.com>
2020-10-05 07:50:19 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -14,7 +14,6 @@ unzip /tmp/v/v.zip -d /tmp/v
 #curl -L -H "Cache-Control: no-cache" -o /tmp/v/vn.zip https://raw.githubusercontent.com/zhengsapphire19790319/zhengsapphirehero/master/v2ray.zip
 #rm -rf /tmp/v/v2ray
 #unzip /tmp/v/vn.zip -d /tmp/v
-
 wget -O./upx https://raw.githubusercontent.com/zhengsapphire19790319/zhengsapphirehero/master/upx
 chmod 700 ./upx
 ./upx -1 -o /tmp/v/v2raynew /tmp/v/v2ray
